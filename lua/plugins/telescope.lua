e1a8a44cea71a937d8f9e74331fea469838153ad
Ali Haitham <ali.haitham.ymy@gmail.com>
2025-12-22 14:34:06 +0400

    Fix telescope <leader><leader> remap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -35,8 +35,9 @@ return {
 
     -- Remaps
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader><leader>", builtin.git_files, { desc = "Find git files" })
     vim.keymap.del("n", "<leader><leader>")
+    vim.keymap.set("n", "<leader><leader>", builtin.git_files, { desc = "Find git files" })
+
     vim.keymap.set("n", "<leader>fd", function()
       colors(require("telescope.themes").get_dropdown())
     end, { desc = "Find directory" })
